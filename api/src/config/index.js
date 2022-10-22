2e916c710e65b72892e961e43ca9db2b03081095
noob-starter <pratikyawalkar5263@gmail.com>
2022-10-22 17:51:36 +0530

    h

---
+++ api/src/config/index.js
@@ -0,0 +1,11 @@
+// src / config / index.js
+'use strict'
+
+const config = {
+  port: 5001,
+  secret: 'super-secret-key',
+  databaseUrl: 'mongodb+srv://pratik:pratik@app.b5b6z9u.mongodb.net/zwitter',
+  saltRounds: 10
+}
+
+module.exports = config
---
+++ frontend/src/components/about.js
@@ -0,0 +1,22 @@
+// Imports
+import React, { Component } from 'react'
+
+class About extends Component {
+  render () {
+    return (
+      <section>
+        <h2><span role="img" aria-label="about">ℹ</span>️ About</h2>
+
+        <br/>
+
+        <p>Grounded and solution-oriented Computer Scientist. Passionate about data science, want to grow and keep up the cutting edge of technologies. Seeking opportunities to work in challenging environment, industry. </p>
+
+        <br/>
+
+        <p>I will give my best-effort knowledge to reach the organization's goals and objectives of the computer science field. To obtain fresher level skills and designing information and ready to enable the company to operate as well as effectively your business.</p>
+      </section>
+    )
+  }
+}
+
+export default About
